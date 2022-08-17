81b4fca99aac578820b554861479f486fe3b037a
YassineOurara <yassineourara@gmail.com>
2022-08-17 18:07:01 +0100

    .

--- src/main/java/com/example/gestacriv/HelloApplication.java
+++ src/main/java/com/example/gestacriv/HelloApplication.java
@@ -3,8 +3,6 @@ package com.example.gestacriv;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.MenuButton;
-import javafx.scene.control.MenuItem;
 import javafx.stage.Stage;
 import java.io.IOException;
 
