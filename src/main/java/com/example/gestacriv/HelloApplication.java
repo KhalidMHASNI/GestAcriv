46db237cbdaeb8c53d52b4278d5d19abf30245a9
Gamareel <kmhasni15@gmail.com>
2022-08-17 16:59:04 +0100

    eez

--- src/main/java/com/example/gestacriv/HelloApplication.java
+++ src/main/java/com/example/gestacriv/HelloApplication.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("home.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Ins  cription.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1540, 800);
         stage.setTitle("Home");
         stage.setScene(scene);
