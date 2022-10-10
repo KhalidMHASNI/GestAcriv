f848fbb9925afca063786d59dd1d9908abc89d34
YassineOurara <yassineourara@gmail.com>
2022-10-10 18:35:34 +0100

    ens

--- src/main/java/com/example/gestacriv/HelloApplication.java
+++ src/main/java/com/example/gestacriv/HelloApplication.java
@@ -10,7 +10,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("drt.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("ens.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1540, 800);
         stage.setTitle("Bonjour");
         stage.setScene(scene);
