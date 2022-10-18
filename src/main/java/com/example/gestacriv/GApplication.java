cd3571378e2ff38de06702a733c743287b48af80
YassineOurara <yassineourara@gmail.com>
2022-10-18 20:25:53 +0100

    labels update

--- src/main/java/com/example/gestacriv/GApplication.java
+++ src/main/java/com/example/gestacriv/GApplication.java
@@ -6,12 +6,12 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class GApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("ens.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1280, 800);
+        FXMLLoader fxmlLoader = new FXMLLoader(GApplication.class.getResource("ens.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1540, 800);
         stage.setTitle("Bonjour");
         stage.setScene(scene);
         stage.show();
